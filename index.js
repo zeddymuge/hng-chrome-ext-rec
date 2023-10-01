f78a1f1017b9b27f479e19598670b8461f806e73
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-10-02 01:02:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+const { promisify } = require('util');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 
 // Configure AWS S3
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -101,7 +105,68 @@ app.get('/api/transcribe/:video_filename', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+function getContentType(key) {
+  const ext = key.split('.').pop().toLowerCase();
+  switch (ext) {
+    case 'mp4':
+      return 'video/mp4';
+    case 'webm':
+      return 'video/webm';
+    default:
+      return 'application/octet-stream';
+  }
+}
+app.get('/api/videos', async (req, res) => {
+  try {
+    const s3 = new AWS.S3();
+    const listObjectsV2 = promisify(s3.listObjectsV2.bind(s3));
+
+    const data = await listObjectsV2({ Bucket: process.env.S3_BUCKET });
+
+    if (data.Contents.length === 0) {
+      return res.json({ message: 'No videos found' });
+    }
+
+    const videos = data.Contents.map((obj) => ({
+      key: obj.Key,
+      url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${obj.Key}`,
+    }));
+
+    res.json(videos);
+  } catch (error) {
+    console.error('Error listing videos:', error);
+    res.status(500).json({ error: 'Error listing videos' });
+  }
+});
+
+// Play a specific video
+app.get('/api/play/:videoKey', (req, res) => {
+  const videoKey = req.params.videoKey;
+  const s3 = new AWS.S3();
+
+  console.log('Fetching video with key:', videoKey);
+
+  const params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: videoKey,
+  };
+
+  const stream = s3.getObject(params).createReadStream();
+
+  // Set the Content-Type header based on the video file extension
+  const contentType = getContentType(videoKey);
+  res.setHeader('Content-Type', contentType);
+
+  stream.pipe(res);
+
+  stream.on('error', (error) => {
+    console.error('Error streaming video:', error);
+    res.status(500).json({ error: 'Error streaming video' });
+  });
+});
+
+
+const PORT = process.env.PORT || 3009;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
